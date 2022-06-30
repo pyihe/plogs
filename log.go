5963034d09b661b04390b37f5e1b58221c320cf6
pyihe <785131182@qq.com>
2022-06-30 15:18:00 +0800

    implements logger

--- log.go
+++ log.go
@@ -1,5 +1,69 @@
 package plogs
 
+func (log *Logger) Panic(args ...interface{}) {
+	message := getMessage("", args)
+	log.log(LevelPanic, message)
+	log.panic(message)
+}
+
+func (log *Logger) Panicf(template string, args ...interface{}) {
+	message := getMessage(template, args)
+	log.log(LevelPanic, message)
+	log.panic(message)
+}
+
+func (log *Logger) Fatal(args ...interface{}) {
+	message := getMessage("", args)
+	log.log(LevelFatal, message)
+	log.exit()
+}
+
+func (log *Logger) Fatalf(template string, args ...interface{}) {
+	message := getMessage(template, args)
+	log.log(LevelFatal, message)
+	log.exit()
+}
+
+func (log *Logger) Error(args ...interface{}) {
+	message := getMessage("", args)
+	log.log(LevelError, message)
+}
+
+func (log *Logger) Errorf(template string, args ...interface{}) {
+	message := getMessage(template, args)
+	log.log(LevelError, message)
+}
+
+func (log *Logger) Warn(args ...interface{}) {
+	message := getMessage("", args)
+	log.log(LevelWarning, message)
+}
+
+func (log *Logger) Warnf(template string, args ...interface{}) {
+	message := getMessage(template, args)
+	log.log(LevelWarning, message)
+}
+
+func (log *Logger) Info(args ...interface{}) {
+	message := getMessage("", args)
+	log.log(LevelInfo, message)
+}
+
+func (log *Logger) Infof(template string, args ...interface{}) {
+	message := getMessage(template, args)
+	defaultLogger.log(LevelInfo, message)
+}
+
+func (log *Logger) Debug(args ...interface{}) {
+	message := getMessage("", args)
+	log.log(LevelDebug, message)
+}
+
+func (log *Logger) Debugf(template string, args ...interface{}) {
+	message := getMessage(template, args)
+	log.log(LevelDebug, message)
+}
+
 func Panic(args ...interface{}) {
 	message := getMessage("", args)
 	defaultLogger.log(LevelPanic, message)
